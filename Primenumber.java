c36fd5223c158906b0eed399db1f1b24d344376c
hemanth17292 <94160878+hemanth17292@users.noreply.github.com>
2022-04-20 15:03:37 +0530

    Update Primenumber.java

--- Primenumber.java
+++ Primenumber.java
@@ -2,7 +2,7 @@ public class Primenumber {
 
     public static void main(String[] args) {
 
-        int num = 29;
+        int num = 28;
         boolean flag = false;
         for(int i = 2; i <= num/2; ++i)
         {
